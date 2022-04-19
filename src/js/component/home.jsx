1da1cffebcda4f395985844510e923dd8693469b
Chris-Durr <chrisdurr88@gmail.com>
2022-04-19 13:19:59 +0000

    1st

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,21 +4,15 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	let number = props.seconds.toString();
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<i className="fa-solid fa-clock"></i>
+			<div className="segundos">{number[number.length - 1]}</div>
+			<div className="minutos">{number[number.length - 2]}</div>
+			<div className="horas">{number[number.length - 3]}</div>
+			<div className="dias">{number[number.length - 4]}</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,10 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+let counter = 0;
+setInterval(function () {
+	console.log(counter);
+	ReactDOM.render(<Home seconds={counter} />, document.querySelector("#app"));
+	counter++;
+}, 1000);
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
